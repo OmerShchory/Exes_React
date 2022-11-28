23275f7c9590726a99a4bbe3624ce95d36f14a33
roeilavie <102984425+roeilavie@users.noreply.github.com>
2022-11-28 16:20:54 +0200

    dssdfsdf

--- Exe2/src/Functional Components/Navber.jsx
+++ Exe2/src/Functional Components/Navber.jsx
@@ -1,17 +1,17 @@
-import Navbar from 'react-bootstrap/Navbar';
-import {shopLogo} from '../Assets/images';
+import Navbar from "react-bootstrap/Navbar";
+import { shopLogo } from "../Assets/images";
 
-export default function Nav(){
-    return( 
-        <Navbar className="navbar">
-            <Navbar.Brand>
-            <img
-                src={shopLogo}
-                style={{width:50, height:50, padding:10, borderRadius: '20px'}}
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-            />
-            </Navbar.Brand>
-        </Navbar>
-    )
+export default function Nav() {
+  return (
+    <Navbar className="navbar">
+      <Navbar.Brand>
+        <img
+          src={shopLogo}
+          style={{ width: 50, height: 50, padding: 10, borderRadius: "20px" }}
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+      </Navbar.Brand>
+    </Navbar>
+  );
 }
